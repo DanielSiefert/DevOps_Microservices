af30645245b02150820c001eee7884ff5c676396
Daniel Siefert <7648401+DanielSiefert@users.noreply.github.com>
2019-11-26 03:52:12 +0000

    Configuring Container Configure a python3 based container to run machine learning app

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag danielsiefert/udacity-aws-devops-ml-app .
 # Step 2: 
 # List docker images
+docker images danielsiefert/udacity-aws-devops-ml-app
 
 # Step 3: 
 # Run flask app
+docker run -p 127.0.0.1:8000:80/tcp danielsiefert/udacity-aws-devops-ml-app
