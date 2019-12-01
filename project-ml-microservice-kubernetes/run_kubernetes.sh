d8387483825e9a2f61011bb3593e69c091840841
Daniel Siefert <7648401+DanielSiefert@users.noreply.github.com>
2019-12-01 12:10:38 -0500

    Update to kubernetes run script.  Create deployment and proxy local port to container.  Recorded result to text file

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=danielsiefert/udacity-aws-devops-ml-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-aws-devops-ml-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
+#Give Container 15 seconds to start
+echo "Waiting for container to start..."
+sleep 15
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacity-aws-devops-ml-app 8000:80
